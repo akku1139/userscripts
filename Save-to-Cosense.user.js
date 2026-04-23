90bb98142041047004a089d8531bff4bed1ff388
Akari Tsuyukusa <akkun11.open@gmail.com>
2026-04-23 22:30:59 +0900

    Create Save-to-Cosense.user.js
    https://gist.github.com/akku1139/da9b6e1127b3d419062bd2abb3062812

---
+++ Save-to-Cosense.user.js
@@ -0,0 +1,66 @@
+// ==UserScript==
+// @name        Save to Cosense
+// @description クリックするとCosenseにページを保存します
+// @homepageURL https://github.com/akku1139/userscripts
+// @license     Unlicense
+// @namespace   Violentmonkey Scripts
+// @match       *://*/*
+// @grant       GM_setValue
+// @grant       GM_getValue
+// @grant       GM_registerMenuCommand
+// @grant       GM_openInTab
+// @version     0.2.2
+// @author      akku <akku1139>
+// ==/UserScript==
+
+/* LICENSE
+This is free and unencumbered software released into the public domain.
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+For more information, please refer to <https://unlicense.org/>
+*/
+
+const valuesDefault = {
+  "projectName": "",
+  "tags": "",
+}
+
+const init = () => {
+  Object.entries(valuesDefault).forEach(v => {
+    GM_setValue(v[0], GM_getValue(v[0]) ?? v[1])
+  })
+}
+
+GM_registerMenuCommand("Save to Cosense", () => {
+  init()
+  const projectName = GM_getValue("projectName")
+  if(!projectName) alert("プロジェクト名を設定する必要があります")
+  const tags = GM_getValue("tags").split(" ").filter(Boolean)
+
+  const title = document.title
+  const lines = []
+
+  const sel = window.getSelection().toString()
+
+  if(sel) lines.push(sel, "")
+  lines.push(location.href, "") // URL
+  if(tags) lines.push("#"+tags.join(" #")) // tags
+
+  GM_openInTab(`https://scrapbox.io/${projectName}/${encodeURIComponent(title)}?body=${encodeURIComponent(lines.join('\n'))}`)
+})
